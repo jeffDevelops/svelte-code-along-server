4aee3757a7d4e2630588398114f35950ca7fdd7d
jeffDevelops <jefferydreynolds@gmail.com>
2020-01-24 23:35:44 -0700

    adds build to gitignore

--- build/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var getSvelteFacts_1 = __importDefault(require("./getSvelteFacts"));
-exports.default = [getSvelteFacts_1.default];
--- build/typeDefs/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var SvelteFact_1 = __importDefault(require("./SvelteFact"));
-exports.default = [SvelteFact_1.default];
